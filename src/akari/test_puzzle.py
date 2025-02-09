e46f7cb055753fbe00914fd99116e1309f997d04
Alex C. Szatmary <alex.szatmary@gmail.com>
2025-02-09 17:57:59 -0500

    Fix tests to use x instead of +

--- src/akari/test_puzzle.py
+++ src/akari/test_puzzle.py
@@ -59,11 +59,11 @@ board_1_sol = np.array(list(map(list, board_1_sol_str.split("\n"))), dtype="str"
 
 board_1_sol_illuminated_str = """
 -------
--+#__+-
+-x#__x-
 -||0_#-
 -#2-1|-
--+#3#+-
--++#++-
+-x#3#x-
+-xx#xx-
 -------
 """[1:-1]
 board_1_sol_illuminated = np.array(
@@ -97,22 +97,23 @@ def test_check_number_1():
 
 
 def test_illuminate_1():
-    (wrong_bulb_pairs, illuminated_board) = illuminate(board_1_sol)
+    (wrong_bulb_pairs, illuminated_board) = illuminate(board_1_sol.copy())
     assert not wrong_bulb_pairs
-    print(illuminated_board)
-    print()
-    print(board_1_sol_illuminated)
     assert np.all(illuminated_board == board_1_sol_illuminated)
     board_1_sol_wrong = board_1_sol.copy()
     board_1_sol_wrong[2, 1] = "#"
     board_1_sol_wrong[1, 5] = "#"
-    (wrong_bulb_pairs_wrong, illuminated_board_wrong) = illuminate(board_1_sol_wrong)
+    (wrong_bulb_pairs_wrong, illuminated_board_wrong) = illuminate(
+        board_1_sol_wrong.copy()
+    )
     board_1_sol_illuminated_wrong = board_1_sol_illuminated.copy()
     board_1_sol_illuminated_wrong[2, 1] = "#"
-    board_1_sol_illuminated_wrong[2, 2] = "+"
+    board_1_sol_illuminated_wrong[2, 2] = "x"
     board_1_sol_illuminated_wrong[1, 5] = "#"
-    print(wrong_bulb_pairs_wrong)
     assert wrong_bulb_pairs_wrong == [(1, 2, 1, 5), (1, 5, 2, 5), (2, 1, 3, 1)]
+    print(illuminated_board_wrong)
+    print(board_1_sol_illuminated_wrong)
+    print(illuminated_board_wrong == board_1_sol_illuminated_wrong)
     assert np.all(illuminated_board_wrong == board_1_sol_illuminated_wrong)
 
 
